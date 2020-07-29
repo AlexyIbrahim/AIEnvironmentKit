4bb1b2b7ca7e4a41f9505bde0f674a01881ae04d
Alexy <>
2020-07-29 14:18:49 +0300

    added example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Alexy Ibrahim on 7/29/20.
+//  Copyright © 2020 Alexy Ibrahim. All rights reserved.
+//
+
+import UIKit
+import AIEnvironmentKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label_Environment:UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let env: String = AIEnvironmentKit.environmentName
+        self.label_Environment.text = env
+    }
+
+
+}
+
--- Sources/AIEnvironmentKit/AIEnvironmentKit.swift
+++ Sources/AIEnvironmentKit/AIEnvironmentKit.swift
@@ -1,84 +1,62 @@
 import Foundation
 import UIKit
+import SystemConfiguration
+import QuartzCore
+//import SwiftOverlayShims
 
-enum AIEnvironment {
+public enum AIEnvironment {
     case simulator
     case debug
     case adhoc
+    case testfligt
     case appStore
-    case cableBuild
+    case other
 }
 
 public class AIEnvironmentKit {
-    @objc static var isCableBuild: Bool = {
-        let currentState = UIDevice.current.batteryState
-        if currentState == .charging || currentState == .full {
-            if AIEnvironmentKit.isDebug {
-                return true
-            }
-        }
-        return false
-    }()
-    
-    @objc static var isDebug: Bool = {
-        #if DEBUG
-        return true
-        #else
-        return false
-        #endif
-    }()
-    
-//    @objc static var isRunningInTestFlightEnvironment: Bool = {
-//        if isSimulator() {
-//            return false
-//        } else {
-//            if isAppStoreReceiptSandbox() && !hasEmbeddedMobileProvision() {
-//                return true
-//            } else {
-//                return false
-//            }
-//        }
-//    }()
-    
-    @objc static var isAdHoc: Bool = {
-        if !AIEnvironmentKit.isDebug && AIEnvironmentKit.isDebugOrAdhoc {
-            return true
-        }
-        return false
-    }()
-    
-    @objc static var isRelease: Bool = {
-        #if !DEBUG
-        return true
-        #else
-        return false
-        #endif
-    }()
-    
-    @objc static var isDebugOrAdhoc: Bool = {
+    // MARK: - environment
+    public static var environment: AIEnvironment = {
         if AIEnvironmentKit.isSimulator {
-            return true
-        } else {
-            if AIEnvironmentKit.hasEmbeddedMobileProvision {
-                return true
-            }
-            return false
+            return .simulator
         }
+        
+        if AIEnvironmentKit.isDebug {
+            return .debug
+        }
+        
+        if AIEnvironmentKit.isAdHoc {
+            return .adhoc
+        }
+        
+        if AIEnvironmentKit.isTestflight {
+            return .testfligt
+        }
+        
+        if AIEnvironmentKit.isAppStore {
+            return .appStore
+        }
+        
+        return .other
     }()
     
-    
-    
-    @objc static var environmentName: String {
-        if AIEnvironmentKit.isDebug {
+    @objc public static var environmentName: String {
+        switch AIEnvironmentKit.environment {
+        case .simulator:
+            return "simulator"
+        case .debug:
             return "debug"
-        } else if AIEnvironmentKit.isAdHoc {
+        case .adhoc:
             return "adhoc"
-        } else {
-            return "release"
+        case .testfligt:
+            return "testflight"
+        case .appStore:
+            return "appStore"
+        case .other:
+            return "other"
         }
     }
     
-    @objc static var isSimulator: Bool = {
+    @objc public static var isSimulator: Bool = {
         #if targetEnvironment(simulator) // #if TARGET_OS_SIMULATOR
         return true
         #else
@@ -86,25 +64,47 @@ public class AIEnvironmentKit {
         #endif
     }()
     
-    func bit_currentAppEnvironment() -> BITEnvironment {
+    @objc public static var isDebug: Bool = {
         if AIEnvironmentKit.isSimulator {
-            return BITEnvironmentOther
+            return true
         } else {
-            
-            // MobilePovision profiles are a clear indicator for Ad-Hoc distribution
-            if bit_hasEmbeddedMobileProvision() {
-                return BITEnvironmentOther
-            }
-            
-            if bit_isAppStoreReceiptSandbox() {
-                return BITEnvironmentTestFlight
-            }
-            
-            return BITEnvironmentAppStore
+            #if DEBUG
+            return true
+            #else
+            return false
+            #endif
         }
-    }
+    }()
     
-    @objc static var isAppStore: Bool = {
+    @objc public static var isAdHoc: Bool = {
+        if AIEnvironmentKit.isSimulator {
+            return false
+        } else {
+            if AIEnvironmentKit.hasEmbeddedMobileProvision {
+                return true
+            }
+            return false
+        }
+        
+        //        if !AIEnvironmentKit.isDebug && AIEnvironmentKit.isDebugOrAdhoc {
+        //            return true
+        //        }
+        //        return false
+    }()
+    
+    @objc public static var isTestflight: Bool = {
+        if AIEnvironmentKit.isSimulator {
+            return false
+        } else {
+            if AIEnvironmentKit.isAppStoreReceiptSandbox && !AIEnvironmentKit.hasEmbeddedMobileProvision {
+                return true
+            } else {
+                return false
+            }
+        }
+    }()
+    
+    @objc public static var isAppStore: Bool = {
         if AIEnvironmentKit.isSimulator {
             return false
         } else {
@@ -116,7 +116,79 @@ public class AIEnvironmentKit {
         }
     }()
     
-    @objc static var isAppStoreReceiptSandbox: Bool = {
+    @objc public static var isRelease: Bool = {
+        if AIEnvironmentKit.isSimulator {
+            return false
+        } else {
+            #if !DEBUG
+            return true
+            #else
+            return false
+            #endif
+        }
+    }()
+    
+    // MARK: - misc
+    @objc public static let isRunningInAppExtension: Bool = {
+        var isRunningInAppExtension = (Bundle.main.executablePath as NSString?)?.range(of: ".appex/").location != NSNotFound
+        return isRunningInAppExtension
+    }()
+    
+    @objc public static let isDebuggerAttached: Bool = {
+        var debuggerIsAttached = false
+        
+        var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        var info: kinfo_proc = kinfo_proc()
+        var info_size = MemoryLayout<kinfo_proc>.size
+        
+        let success = name.withUnsafeMutableBytes { (nameBytePtr: UnsafeMutableRawBufferPointer) -> Bool in
+            guard let nameBytesBlindMemory = nameBytePtr.bindMemory(to: Int32.self).baseAddress else { return false }
+            return -1 != sysctl(nameBytesBlindMemory, 4, &info, &info_size, nil, 0)
+        }
+        
+        if !success {
+            debuggerIsAttached = false
+        }
+        
+        if !debuggerIsAttached && (info.kp_proc.p_flag & P_TRACED) != 0 {
+            debuggerIsAttached = true
+        }
+        
+        return debuggerIsAttached
+        
+        //        var info = kinfo_proc()
+        //        var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        //        var size = MemoryLayout<kinfo_proc>.stride
+        //        let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+        //        assert(junk == 0, "sysctl failed")
+        //        return (info.kp_proc.p_flag & P_TRACED) != 0
+    }()
+    
+    @objc public static let isCableConnected: Bool = {
+        let currentState = UIDevice.current.batteryState
+        if currentState == .charging || currentState == .full {
+            return true
+        }
+        return false
+    }()
+    
+    
+    // MARK: - other / private
+    // 🌿 MobilePovision profiles are a clear indicator for Ad-Hoc distribution
+    @objc private static var hasEmbeddedMobileProvision: Bool = {
+        //        if let _ = Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") {
+        //            return true
+        //        }
+        //        return false
+        
+        let data = NSData(contentsOfFile: Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") ?? "") as Data?
+        guard data != nil else {
+            return false
+        }
+        return true
+    }()
+    
+    @objc private static var isAppStoreReceiptSandbox: Bool = {
         if AIEnvironmentKit.isSimulator {
             return false
         } else {
@@ -131,24 +203,13 @@ public class AIEnvironmentKit {
         }
     }()
     
-    @objc static var hasEmbeddedMobileProvision: Bool = {
-        //        if let _ = Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") {
-        //            return true
-        //        }
-        //        return false
-        
-        let data = NSData(contentsOfFile: Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") ?? "") as Data?
-        guard data != nil else {
-            return false
-        }
-        return true
-    }()
+    
 }
 
-// 🌿 methods
+// MARK: - public methods
 extension AIEnvironmentKit {
-    @objc final class func executeIfCableBuild(callback: () -> ()) {
-        if AIEnvironmentKit.isCableBuild {
+    @objc final class func executeIfDebuggerAttached(callback: () -> ()) {
+        if AIEnvironmentKit.isDebuggerAttached {
             callback()
         }
     }
@@ -159,23 +220,23 @@ extension AIEnvironmentKit {
         }
     }
     
-    @objc final class func executeIfDebugOrAdhoc(callback: () -> ()) {
-        if AIEnvironmentKit.isDebug || AIEnvironmentKit.isAdHoc {
+    @objc final class func executeIfAdhoc(callback: () -> ()) {
+        if AIEnvironmentKit.isAdHoc {
             callback()
         }
     }
     
-    @objc final class func executeIfRelease(callback: () -> ()) {
-        if AIEnvironmentKit.isRelease {
+    @objc final class func executeIfNotAppStore(callback: () -> ()) {
+        if !AIEnvironmentKit.isAppStore {
             callback()
         }
     }
     
-    @objc final class func execute(debugAdhoc: () -> (), release: () -> ()) {
-        if AIEnvironmentKit.isDebug || AIEnvironmentKit.isAdHoc {
-            debugAdhoc()
+    @objc final class func execute(notAppStore: () -> (), appStore: () -> ()) {
+        if !AIEnvironmentKit.isAppStore {
+            notAppStore()
         } else {
-            release()
+            appStore()
         }
     }
 }
